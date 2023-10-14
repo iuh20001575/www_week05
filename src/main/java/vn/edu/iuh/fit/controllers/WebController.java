bd6e4c32a120c082bd3312537c8ab3c34a90b7c0
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-10-14 09:01:45 +0700

    Config base web and add bootstrap

---
+++ src/main/java/vn/edu/iuh/fit/controllers/WebController.java
@@ -0,0 +1,12 @@
+package vn.edu.iuh.fit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
